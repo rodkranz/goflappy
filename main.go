0e401e85d938b47aa1edd5a666985baa12fa7f78
Rodrigo Lopes <rodrigo.lopes@olx.com>
2018-02-27 17:06:03 +0000

    background painted

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+	
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/ttf"
+	"github.com/veandco/go-sdl2/img"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
+	os.Exit(0)
+}
+
+func run() error {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+	
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("could not initialize TTF: %v", err)
+	}
+	defer ttf.Quit()
+	
+	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("could not create window: %v", err)
+	}
+	defer w.Destroy()
+	
+	if err := drawTitle(r); err != nil {
+		return fmt.Errorf("could not draw title: %v", err)
+	}
+	time.Sleep(3 * time.Second)
+	
+	if err := drawBackground(r); err != nil {
+		return fmt.Errorf("could not draw background: %v", err)
+	}
+	time.Sleep(10 * time.Second)
+	
+	return nil
+}
+
+func drawBackground(r *sdl.Renderer) error {
+	r.Clear()
+	
+	t, err := img.LoadTexture(r, "./res/imgs/background.png")
+	if err != nil {
+		return fmt.Errorf("could not load background image: %v", err)
+	}
+	defer t.Destroy()
+	
+	if err := r.Copy(t, nil, nil); err != nil {
+		return fmt.Errorf("could not copy background: %v", err)
+	}
+	
+	r.Present()
+	return nil
+}
+
+func drawTitle(r *sdl.Renderer) error {
+	r.Clear()
+	
+	f, err := ttf.OpenFont("./res/fonts/FiraCode.ttf", 20)
+	if err != nil {
+		return fmt.Errorf("could not load font: %v", err)
+	}
+	defer f.Close()
+	
+	c := sdl.Color{R: 255, G: 100, B: 0, A: 255}
+	s, err := f.RenderUTF8_Solid("Flappy Gopher", c)
+	if err != nil {
+		return fmt.Errorf("could not render title: %v", err)
+	}
+	defer s.Free()
+	
+	t, err := r.CreateTextureFromSurface(s)
+	if err != nil {
+		return fmt.Errorf("could not create texture: %v", err)
+	}
+	defer t.Destroy()
+	
+	if err := r.Copy(t, nil, nil); err != nil {
+		return fmt.Errorf("could not copy texture: %v", err)
+	}
+	
+	r.Present()
+	
+	return nil
+}
